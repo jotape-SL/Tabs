268845e77935372b7b19f99b66b32a12aa171c91
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-07-10 11:41:19 -0300

    initial states

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react'
-import { FaAngleDoubleRight } from 'react-icons/fa'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
+import React, { useState, useEffect } from 'react';
+import { FaAngleDoubleRight } from 'react-icons/fa';
+const url = 'https://course-api.com/react-tabs-project';
 function App() {
-  return <h2>tabs project setup</h2>
+  const [isLoading, setIsLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+  return <h2>tabs project setup</h2>;
 }
 
-export default App
+export default App;
