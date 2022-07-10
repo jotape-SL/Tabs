7e6603123a0273055ac3402d26886871e50068c3
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-07-10 11:45:55 -0300

    made the loading screen and fetched the data

--- src/App.js
+++ src/App.js
@@ -5,6 +5,25 @@ function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [jobs, setJobs] = useState([]);
   const [value, setValue] = useState(0);
+
+  const fetchjobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchjobs();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className='section loaing'>
+        <h1>loading...</h1>
+      </section>
+    );
+  }
   return <h2>tabs project setup</h2>;
 }
 
